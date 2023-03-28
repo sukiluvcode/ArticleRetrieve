638839170e49820135f165880cfa1f0f076c1388
sukiluvcode <luvusoike@icloud.com>
2023-03-28 11:05:19 +0800

    delete some redundant files and modify work_flow_rough file.

--- work_flow_rough.py
+++ work_flow_rough.py
@@ -3,6 +3,7 @@ import json
 import time
 import pandas as pd
 
+# count the run time of a function
 def timeit(func):
     def function(*params):
         start = time.time()
@@ -12,57 +13,53 @@ def timeit(func):
         return ret
     return function
 
+# read json file and convert to csv file
+with open("doi/doi_list copy.json", 'r') as fo:
+    list_diction = json.load(fo)
 
-# with open("doi_list copy.json", 'r') as fo:
-#     list_diction = json.load(fo)
+csd_name = []
+doi = []
+for diction in list_diction:
+    for key, value in diction.items():
+        csd_name.append(key)
+        doi.append(value)
 
-# csd_name = []
-# doi = []
-# for diction in list_diction:
-#     for key, value in diction.items():
-#         csd_name.append(key)
-#         doi.append(value)
+df = pd.DataFrame({'csd_name': csd_name, 'doi': doi})
+with open("doi_list.csv", 'w') as f:
+    df.to_csv(f, index=False)
 
-# df = pd.DataFrame({'csd_name': csd_name, 'doi': doi})
-# with open("doi_list.csv", 'w') as f:
-#     df.to_csv(f, index=False)
+# read the csv file
+with open("doi/doi_list.csv", 'r') as csv:
+    df = pd.read_csv(csv)
+    pd.set_option("display.max_rows", 5)
 
-# with open("doi_list.csv", 'r') as csv:
-#     df = pd.read_csv(csv)
-#     pd.set_option("display.max_rows", 5)
+# create a df to collect each unique doi with the correspond first MOF name
+doi_series_true = df.loc[df.doi.notnull()]
+print(doi_series_true)
 
-# doi_series_true = df.loc[df.doi.notnull()]
-# print(doi_series_true)
-# doi_number = doi_series_true.doi.unique()
-# print(len(doi_number))
+single_name = doi_series_true.groupby('doi').apply(lambda df: df.csd_name.iloc[0])
+print(type(single_name))
 
-# doi_csd_name = doi_series_true.groupby(['doi']).size()
-# print(doi_csd_name)
-
-# single_name = doi_series_true.groupby('doi').apply(lambda df: df.csd_name.iloc[0])
-# print(type(single_name))
-
-# with open("digger_list.csv", "w") as f:
-#     single_name.to_csv(f)
+with open("doi/digger_list.csv", "w") as f:
+    single_name.to_csv(f)
     
 
 """remind that 10.1107 not shows the full content using 
 https://doi.org/[doi] instead, using https://onlinelibrary.wiley.com/iucr/doi/[doi]"""
 
+# shows how to combine groupby and read_csv to create a new dataFrame
+with open("doi/digger_list.csv") as f:
+    df = pd.read_csv(f)
+    df = df.rename(columns={'0': "csd_name"})
 
-with open("doi/digger_list.csv") as csv:
-    series = pd.read_csv(csv)
+df.to_csv("doi&csd_name.csv")
 
-prefix_1006 = series.doi.map(lambda prefix: '10.1006' in prefix).sum()
-# print(prefix_1006)
+# filter specify doi prefix
+with open("doi&csd_name.csv") as f:
+    df = pd.read_csv(f, index_col=0)
 
-series = series[~series.doi.str.startswith(('10.1006','10.1071', '10.1055', '10.1080'))]
-
-
-
-# print(series)
-
-prefixes = series.doi.str.extract(r'(\d+\.\d+)')[0]
-group = series.groupby(prefixes).size()
-print(group)
-
+mask = df.doi.str.contains("10.1039")
+RSC_series = df[mask]
+RSC_series.reset_index(drop=True)
+print(RSC_series)
+RSC_series.to_csv("RSCdoi&csd_name.csv")
