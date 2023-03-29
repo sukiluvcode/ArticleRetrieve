abed954cc8010fad93045fe017a754c5ed618d80
sukiluvcode <luvusoike@icloud.com>
2023-03-29 10:25:22 +0800

    formatting content

--- retrieve.py
+++ retrieve.py
@@ -1,16 +1,18 @@
-from downloader.articledownloader import ArticleDownloader
-import pandas as pd
+"""To get the file path"""
 import os
-import time 
+import time
+import pandas as pd
+from downloader.articledownloader import ArticleDownloader
 
 def download(doi, file_name):
+    """download the file with given doi"""
     downloader = ArticleDownloader()
     cwd = os.getcwd()
     file_path = os.path.join(cwd, f"Publication/{file_name}.html")
     my_file = open(file_path, 'wb')
     downloader.get_html_from_doi(doi=doi, writefile=my_file, mode="rsc")
 
-with open("doi/RSCdoi&csd_name.csv") as f:
+with open("doi/RSCdoi&csd_name.csv", encoding="utf-8") as f:
     RSC = pd.read_csv(f, index_col=0)
 
 doi_list = list(RSC.doi)
